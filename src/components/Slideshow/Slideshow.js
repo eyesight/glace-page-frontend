2f1bded1a04dd8a8407da6224445190ceb9739d1
Claudia Friedrich <claudae_f@hotmail.com>
2022-01-13 00:07:29 +0100

    slideshow styling finished

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -18,39 +18,52 @@ const Slideshow = () => {
     // Create array with 1000 slides
     const slides = Array.from({ length: 10 }).map(
         (el, index) => (
-            <div key={index} className="swiper-slide" style={{ backgroundImage: 'url(' + 'https://images.unsplash.com/photo-1500643752441-4dc90cda350a?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85' + ')' }}>
-                <img src="https://images.unsplash.com/photo-1500643752441-4dc90cda350a?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85" className="entity-img" />
-                <div className="content">
-                    <p className="title" data-swiper-parallax="-30%" data-swiper-parallax-scale=".7">Alexis Berry</p>
-                    <span className="caption" data-swiper-parallax="-20%">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+            <div key={index} className="swiper__slide">
+                <figure className='swiper__image-wrapper'>
+                    <img className='swiper__image' src="https://images.unsplash.com/photo-1500643752441-4dc90cda350a?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85" />
+                </figure>
+                <div className='swiper__content'>
+                    <a className='swiper__anchor'>
+                        <h2 className='title-h1 swiper__title'>ich bin ein Titel</h2>
+                        <ul class="tags">
+                            <li class="tags__item">winter</li>
+                            <li class="tags__item">herbst</li>
+                            <li class="tags__item">schnell</li>
+                            <li class="tags__item">rahmglace</li>
+                            <li class="tags__item">schokoladig</li>
+                        </ul>
+                    </a>
                 </div>
-            </div>)
+            </div>
+        )
     );
 
     return (
-        <Swiper
-            init={false}
-            loop={true}
-            speed={800}
-            slidesPerView={'auto'}
-            centeredSlides={true}
-            effect={'coverflow'}
-            coverflowEffect={{
-                'rotate': 0,
-                'stretch': -80,
-                'depth': 100,
-                'modifier': 3,
-                'slideShadows': true,
-            }}
-            grabCursor={true}
-            parallax={true}
-            className="swiper-container loading swiper-container-3d">
-            {slides.map((slideContent, index) => (
-                <SwiperSlide key={index + 'slide'} virtualIndex={index} className='swiper-slide'>
-                    {slideContent}
-                </SwiperSlide>
-            ))}
-        </Swiper>
+        <section className='section section--swiper'>
+            <Swiper
+                init={false}
+                loop={true}
+                speed={800}
+                slidesPerView={'auto'}
+                centeredSlides={true}
+                effect={'coverflow'}
+                coverflowEffect={{
+                    'rotate': 0,
+                    'stretch': 20,
+                    'depth': 100,
+                    'modifier': 5,
+                    'slideShadows': false,
+                }}
+                grabCursor={true}
+                parallax={true}
+                className="swiper__container">
+                {slides.map((slideContent, index) => (
+                    <SwiperSlide key={index + 'slide'} virtualIndex={index} className='swiper-slide'>
+                        {slideContent}
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </section>
     );
 };
 
